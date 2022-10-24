667979245225ff46b070854b101ae14636972064
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-10-24 20:52:12 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const questions = [
+    {
+        questionText: 'Когда родился Гарри Поттер?',
+        answerOptions: [
+          { answerText: '31 июля 1980', isCorrect: true },
+          { answerText: '31 августа 1980', isCorrect: false },
+          { answerText: '30 июля 1980', isCorrect: false }
+        ],
+    },
+    {
+      questionText: 'Какого привидения никогда не было в Хогвартсе?',
+      answerOptions: [
+        { answerText: 'Пивз', isCorrect: false },
+        { answerText: 'Друри Лейн', isCorrect: true },
+        { answerText: 'Бинс', isCorrect: false }
+      ],
+    },
+    {
+      questionText: 'Как попасть в гостиную Когтервана?',
+      answerOptions: [
+        { answerText: 'Повторить стук в определённом ритме', isCorrect: true },
+        { answerText: 'Пароль', isCorrect: false },
+        { answerText: 'Ответить на вопрос', isCorrect: false }
+      ],
+    },
+    {
+      questionText: 'Полное имя Альбуса Дамблдора?',
+      answerOptions: [
+        { answerText: 'Альбус Персиваль Валентайн Брайан Дамблдор', isCorrect: false },
+        { answerText: 'Альбус Персиваль Вилберн Брайн Дамблдор', isCorrect: false },
+        { answerText: 'Альбус Персиваль Вульфрик Брайан Дамблдор', isCorrect: true }
+      ],
+    },
+    {
+      questionText: 'Какую форму принимает патронус Джинни Уизли?',
+      answerOptions: [
+        { answerText: 'Выдра', isCorrect: false },
+        { answerText: 'Конь', isCorrect: true },
+        { answerText: 'Олень', isCorrect: false }
+      ],
+    },
+  ];
+
+  return (
+    <div className="App">
+      { false ? (
+        <div className='score-section'>Ваш результат ?? из {questions.length}</div>
+      ) : (
+        <>
+          <div className='question-section'>
+            <div className='question-count'>
+              <span>Вопрос 1</span>/{questions.length}
+            </div>
+            <div className='question-text'>{questions[0].questionText}</div>
+          </div>
+          <div className='answer-section'>
+            {questions[0].answerOptions.map((answerOption) => <button>{answerOption.answerText}</button>)}
+          </div>
+        </>
+      )
+      
+    
+    
+    }
+    </div>
+  );
+}
+
+export default App;
